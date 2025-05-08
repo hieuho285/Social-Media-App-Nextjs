c290fd86251dce57f37a7a9a2a77fa083cdc1a59
Tony <hieuho285@gmail.com>
2025-05-08 11:32:12 +0700

    starting code

---
+++ src/app/users/[id]/page.tsx
@@ -0,0 +1,33 @@
+import Form from "next/form";
+import Link from "next/link";
+
+type Params = Promise<{
+  id: string;
+}>;
+type SearchParams = Promise<{
+  [key: string]: string | string[] | undefined;
+}>;
+
+export default async function UserPage({
+  params,
+  searchParams,
+}: {
+  params: Params;
+  searchParams: SearchParams;
+}) {
+  const { id } = await params;
+  const { q } = await searchParams;
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">User ID: {id}</h1>
+      <p>Search Query: {q}</p>
+
+      <Form action="">
+        <input type="text" name="q" />
+        <button type="submit">Submit</button>
+      </Form>
+      <Link href="/">Go to Home</Link>
+    </div>
+  );
+}
